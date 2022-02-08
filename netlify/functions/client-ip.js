fc24c23b5b83f4c3bb83f5f9a7a97a1f7937b178
nicgoodman-em <85691335+nicgoodman-em@users.noreply.github.com>
2022-02-08 18:23:24 -0500

    edited test function

--- netlify/functions/client-ip.js
+++ netlify/functions/client-ip.js
@@ -1,18 +1,18 @@
-export async function get() {
+exports.handler = async (event, _, callback) => {
   const res = await fetch(
-      'https://api.notion.com/v1/databases/2063e4db5bfa4f0f92c0e350a64f29d8?v=a4b8579bf9524854911f8c679bf7fbe0',
-      {
-        method: 'GET',
-        headers: {
-          'Notion-Version': '2021-05-13',
-          Authorization: 'Bearer secret_SzoD73V9go2wve3Wx7RTUglFYwwIGXNwtwAZK856RZ5'
-        },
-        redirect: 'follow'
-      }
-    );
-    const entries = await res.json();
-    return {
-      status: 200,
-      body: { entries }
-    };
-  }
+    'https://api.notion.com/v1/databases/2063e4db5bfa4f0f92c0e350a64f29d8?v=a4b8579bf9524854911f8c679bf7fbe0',
+    {
+      method: 'GET',
+      headers: {
+        'Notion-Version': '2021-05-13',
+        Authorization: 'Bearer secret_SzoD73V9go2wve3Wx7RTUglFYwwIGXNwtwAZK856RZ5'
+      },
+      redirect: 'follow'
+    }
+  );
+  const entries = await res.json();
+  return {
+    status: 200,
+    body: { entries }
+  };
+  };
