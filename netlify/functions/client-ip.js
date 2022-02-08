247c361eaaf34a5792cd95904c8bb5a098b579ea
nicgoodman-em <85691335+nicgoodman-em@users.noreply.github.com>
2022-02-08 18:20:23 -0500

    testing fetch in netlify function

--- netlify/functions/client-ip.js
+++ netlify/functions/client-ip.js
@@ -1,8 +1,18 @@
-exports.handler = async (event, _, callback) => {
-    const ip = event.headers['x-forwarded-for'];
-    const client = ip.split(',')[0];
-    callback(null, {
-      statusCode: 200,
-      body: client,
-    });
-  };
+export async function get() {
+  const res = await fetch(
+      'https://api.notion.com/v1/databases/2063e4db5bfa4f0f92c0e350a64f29d8?v=a4b8579bf9524854911f8c679bf7fbe0',
+      {
+        method: 'GET',
+        headers: {
+          'Notion-Version': '2021-05-13',
+          Authorization: 'Bearer secret_SzoD73V9go2wve3Wx7RTUglFYwwIGXNwtwAZK856RZ5'
+        },
+        redirect: 'follow'
+      }
+    );
+    const entries = await res.json();
+    return {
+      status: 200,
+      body: { entries }
+    };
+  }
