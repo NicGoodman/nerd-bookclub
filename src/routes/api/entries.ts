10ea12b9e93421043f1db4e28aeb5f03f9f8c5b5
nicgoodman-em <85691335+nicgoodman-em@users.noreply.github.com>
2022-02-08 19:17:25 -0500

    added environmental variables

--- src/routes/api/entries.ts
+++ src/routes/api/entries.ts
@@ -1,15 +1,13 @@
 export async function get() {
-const res = await fetch(
-		'https://api.notion.com/v1/databases/2063e4db5bfa4f0f92c0e350a64f29d8?v=a4b8579bf9524854911f8c679bf7fbe0',
-		{
-			method: 'GET',
-			headers: {
-				'Notion-Version': '2021-05-13',
-				Authorization: 'Bearer secret_SzoD73V9go2wve3Wx7RTUglFYwwIGXNwtwAZK856RZ5'
-			},
-			redirect: 'follow'
-		}
-	);
+	// @ts-ignore
+	const res = await fetch(import.meta.env.VITE_ENDPOINT, {
+		method: 'GET',
+		headers: {
+			'Notion-Version': '2021-05-13',
+			Authorization: 'Bearer ' + import.meta.env.VITE_AUTH_TOKEN
+		},
+		redirect: 'follow'
+	});
 	const entries = await res.json();
 	return {
 		status: 200,
