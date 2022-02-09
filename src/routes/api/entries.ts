dff98479ae9b92f1b60ed2f2dd0ea50d882190d7
nicgoodman-em <85691335+nicgoodman-em@users.noreply.github.com>
2022-02-08 20:12:47 -0500

    changed variable to database id

--- src/routes/api/entries.ts
+++ src/routes/api/entries.ts
@@ -1,5 +1,6 @@
 export async function get() {
-	const res = await fetch(`${import.meta.env.VITE_ENDPOINT}`, {
+	const endpoint = `https://api.notion.com/v1/databases/${import.meta.env.VITE_DATABASE_ID}`
+	const res = await fetch(endpoint, {
 		method: 'GET',
 		headers: {
 			'Notion-Version': '2021-05-13',
