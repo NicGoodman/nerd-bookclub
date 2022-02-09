885f51b2968a4fe482c8b19b2c4c028a7f595860
nicgoodman-em <85691335+nicgoodman-em@users.noreply.github.com>
2022-02-08 19:31:53 -0500

    testing env in js instead of typescript

---
+++ src/routes/api/entries.js
@@ -0,0 +1,16 @@
+export async function get() {
+	// @ts-ignore
+	const res = await fetch(import.meta.env.VITE_ENDPOINT, {
+		method: 'GET',
+		headers: {
+			'Notion-Version': '2021-05-13',
+			Authorization: 'Bearer ' + import.meta.env.VITE_AUTH_TOKEN
+		},
+		redirect: 'follow'
+	});
+	const entries = await res.json();
+	return {
+		status: 200,
+		body: { entries }
+	};
+}
